8433b54b6b4ff8daac59051763cb2e8ccdba3d0b
何恺君 <kytonhe@macbook-8.local>
2019-02-27 22:08:20 -0500

    Add details page and superhero page

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by 何恺君 on 2/26/19.
+//  Copyright © 2019 Kaijun He. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
